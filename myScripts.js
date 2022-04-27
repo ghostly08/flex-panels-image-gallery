cb20f54dd03e8163d84629973f699149998d7473
ghostly08 <steven.produccionbcn@gmail.com>
2022-04-27 10:20:58 +0200

    selecting all panels creting events and function

--- myScripts.js
+++ myScripts.js
@@ -1,5 +1,15 @@
-const panels = document.querySelectorAll('.panels');
+const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-  
+  this.classList.toggle('open');
 }
+
+function toggleActive(e) {
+  console.log(e.propertyName); //To see the properties using in the event.
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
